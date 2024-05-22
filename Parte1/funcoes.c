8cccd3fcd3d9bb1b16b45fd62a143913836b5feb
PedroDanielCosta <32197800+PedroDanielCosta@users.noreply.github.com>
2024-05-22 21:32:55 +0100

    Update to read and write

--- Parte1/funcoes.c
+++ Parte1/funcoes.c
@@ -11,79 +11,100 @@
 #include <dirent.h>
 #include <pwd.h>
 
-void mostraFicheiro(const char *ficheiro) {
-    int fd;
-    ssize_t bytesPorLeitura;
-    char buffer[BUFFER_SIZE];
 
-    fd = open(ficheiro, O_RDONLY);
+// Funções Auxiliares
+void write_string(const char *str) {
+    write(1, str, strlen(str));
+}
+
+void read_string(char *buffer, size_t size) {
+    ssize_t bytesRead = read(0, buffer, size - 1);
+    if (bytesRead > 0) {
+        buffer[bytesRead - 1] = '\0';
+    }
+}
+
+void read_int(int *number) {
+    char buffer[BUFFER_SIZE];
+    read_string(buffer, BUFFER_SIZE);
+    *number = atoi(buffer);
+}
+
+// =============
+
+
+// Açõoes
+
+void mostraFicheiro(const char *ficheiro) {
+    int fd = open(ficheiro, O_RDONLY);
     if (fd == -1) {
-        perror("open");
+        perror("Erro ao abrir o arquivo");
         exit(EXIT_FAILURE);
     }
 
-    while ((bytesPorLeitura = read(fd, buffer, BUFFER_SIZE)) > 0) {
-        if (write(STDOUT_FILENO, buffer, bytesPorLeitura) != bytesPorLeitura) {
-            fprintf(stderr, "Erro ao escrever no ecrã\n");
+    char buffer[BUFFER_SIZE];
+    ssize_t bytesLidos;
+    while ((bytesLidos = read(fd, buffer, BUFFER_SIZE)) > 0) {
+        ssize_t bytesEscritos = write(STDOUT_FILENO, buffer, bytesLidos);
+        if (bytesEscritos != bytesLidos) {
+            perror("Erro ao escrever no ecrã");
             exit(EXIT_FAILURE);
         }
     }
 
-    if (bytesPorLeitura == -1) {
-        perror("read");
+    if (bytesLidos == -1) {
+        perror("Erro ao ler do arquivo");
         exit(EXIT_FAILURE);
     }
 
     if (close(fd) == -1) {
-        perror("close");
+        perror("Erro ao fechar o arquivo");
         exit(EXIT_FAILURE);
     }
 }
 
 void copiaFicheiro(const char *ficheiro) {
-    int fdIn, fdOut;
-    ssize_t bytesPorLeitura, bytesPorEscrita;
-    char buffer[BUFFER_SIZE];
-    char nomeCopia[PATH_MAX];
-
-    fdIn = open(ficheiro, O_RDONLY);
+    int fdIn = open(ficheiro, O_RDONLY);
     if (fdIn == -1) {
-        perror("open");
+        perror("Erro ao abrir o arquivo de origem");
         exit(EXIT_FAILURE);
     }
 
+    char nomeCopia[PATH_MAX];
     snprintf(nomeCopia, PATH_MAX, "%s.copia", ficheiro);
 
-    fdOut = open(nomeCopia, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+    int fdOut = open(nomeCopia, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
     if (fdOut == -1) {
-        perror("open");
+        perror("Erro ao abrir o arquivo de destino");
         exit(EXIT_FAILURE);
     }
 
+    ssize_t bytesPorLeitura, bytesPorEscrita;
+    char buffer[BUFFER_SIZE];
     while ((bytesPorLeitura = read(fdIn, buffer, BUFFER_SIZE)) > 0) {
         bytesPorEscrita = write(fdOut, buffer, bytesPorLeitura);
         if (bytesPorEscrita != bytesPorLeitura) {
-            fprintf(stderr, "Erro ao escrever no ficheiro cópia\n");
+            perror("Erro ao escrever no arquivo de destino");
             exit(EXIT_FAILURE);
         }
     }
 
     if (bytesPorLeitura == -1) {
-        perror("read");
+        perror("Erro ao ler do arquivo de origem");
         exit(EXIT_FAILURE);
     }
 
     if (close(fdIn) == -1) {
-        perror("close");
+        perror("Erro ao fechar o arquivo de origem");
         exit(EXIT_FAILURE);
     }
 
     if (close(fdOut) == -1) {
-        perror("close");
+        perror("Erro ao fechar o arquivo de destino");
         exit(EXIT_FAILURE);
     }
 
-    printf("O ficheiro %s foi copiado para %s\n", ficheiro, nomeCopia);
+    write_string("O ficheiro %s foi copiado para %s\n", ficheiro, nomeCopia);
 }
 
 void acrescentaFicheiro(const char *origem, const char *destino) {
@@ -93,39 +114,39 @@ void acrescentaFicheiro(const char *origem, const char *destino) {
 
     fdOrigem = open(origem, O_RDONLY);
     if (fdOrigem == -1) {
-        fprintf(stderr, "Erro ao abrir o ficheiro origem: %s\n", strerror(errno));
+        perror(stderr, "Erro ao abrir o ficheiro origem: %s\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
 
     fdDestino = open(destino, O_WRONLY | O_APPEND);
     if (fdDestino == -1) {
-        fprintf(stderr, "Erro ao abrir o ficheiro destino: %s\n", strerror(errno));
+        perror(stderr, "Erro ao abrir o ficheiro destino: %s\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
 
     while ((bytesPorLeitura = read(fdOrigem, buffer, BUFFER_SIZE)) > 0) {
         bytesPorEscrita = write(fdDestino, buffer, bytesPorLeitura);
         if (bytesPorEscrita != bytesPorLeitura) {
-            fprintf(stderr, "Erro ao escrever no ficheiro destino: %s\n", strerror(errno));
+            perror(stderr, "Erro ao escrever no ficheiro destino: %s\n", strerror(errno));
             exit(EXIT_FAILURE);
         }
     }
 
     if (bytesPorLeitura == -1) {
-        fprintf(stderr, "Erro ao ler do ficheiro origem: %s\n", strerror(errno));
+        perror(stderr, "Erro ao ler do ficheiro origem: %s\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
     if (close(fdOrigem) == -1) {
-        fprintf(stderr, "Erro ao fechar o ficheiro origem: %s\n", strerror(errno));
+        perror(stderr, "Erro ao fechar o ficheiro origem: %s\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
 
     if (close(fdDestino) == -1) {
-        fprintf(stderr, "Erro ao fechar o ficheiro destino: %s\n", strerror(errno));
+        perror(stderr, "Erro ao fechar o ficheiro destino: %s\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
 
-    printf("O ficheiro %s foi acrescentado ao ficheiro %s\n", origem, destino);
+    write("O ficheiro %s foi acrescentado ao ficheiro %s\n", origem, destino);
 }
 
 int contaFicheiro(char *ficheiro) {
@@ -169,7 +190,7 @@ void removeFicheiro(const char *ficheiro) {
 int informaFicheiro(char *filename) {
     struct stat file_stat;
     if (stat(filename, &file_stat) == -1) {
-        fprintf(stderr, "Erro ao obter informações do arquivo: %s\n", strerror(errno));
+        perror(stderr, "Erro ao obter informações do arquivo: %s\n", strerror(errno));
         return 1;
     }
 
@@ -186,7 +207,7 @@ int informaFicheiro(char *filename) {
 
     struct passwd *pw = getpwuid(file_stat.st_uid);
     if (pw == NULL) {
-        fprintf(stderr, "Erro ao obter informações do dono do arquivo: %s\n", strerror(errno));
+        perror(stderr, "Erro ao obter informações do dono do arquivo: %s\n", strerror(errno));
         return 1;
     }
 
@@ -195,13 +216,13 @@ int informaFicheiro(char *filename) {
     strftime(modified_time, sizeof(modified_time), "%b %d %Y %H:%M:%S", localtime(&file_stat.st_mtime));
     strftime(accessed_time, sizeof(accessed_time), "%b %d %Y %H:%M:%S", localtime(&file_stat.st_atime));
 
-    printf("%s:\n", filename);
-    printf("Tipo: %s\n", file_type);
-    printf("Número I-node: %ld\n", (long) file_stat.st_ino);
-    // printf("Dono: %s\n", pw->pw_name);
-    printf("Criado em: %s\n", created_time);
-    printf("Última modificação: %s\n", modified_time);
-    printf("Último acesso: %s\n", accessed_time);
+    write("%s:\n", filename);
+    write("Tipo: %s\n", file_type);
+    write("Número I-node: %ld\n", (long) file_stat.st_ino);
+    write("Dono: %s\n", pw->pw_name);
+    write("Criado em: %s\n", created_time);
+    write("Última modificação: %s\n", modified_time);
+    write("Último acesso: %s\n", accessed_time);
 
     return 0;
 }
@@ -209,7 +230,7 @@ int informaFicheiro(char *filename) {
 int lista(char *dir_name) {
     DIR *dir = opendir(dir_name);
     if (dir == NULL) {
-        fprintf(stderr, "Erro ao abrir diretoria: %s\n", strerror(errno));
+        perror(stderr, "Erro ao abrir diretoria: %s\n", strerror(errno));
         return 1;
     }
 
@@ -220,19 +241,19 @@ int lista(char *dir_name) {
 
         struct stat file_stat;
         if (stat(path, &file_stat) == -1) {
-            fprintf(stderr, "Erro ao obter informações do arquivo: %s\n", strerror(errno));
+            perror(stderr, "Erro ao obter informações do arquivo: %s\n", strerror(errno));
             return 1;
         }
 
         if (S_ISDIR(file_stat.st_mode)) {
-            printf("%s [Pasta]\n", entry->d_name);
+            write("%s [Pasta]\n", entry->d_name);
         } else {
-            printf("%s\n", entry->d_name);
+            write("%s\n", entry->d_name);
         }
     }
 
     if (closedir(dir) == -1) {
-        fprintf(stderr, "Erro ao fechar diretoria: %s\n", strerror(errno));
+        perror(stderr, "Erro ao fechar diretoria: %s\n", strerror(errno));
         return 1;
     }
 
--- Parte1/funcoes.h
+++ Parte1/funcoes.h
@@ -1,7 +1,15 @@
 #ifndef FUNCOES_H
 #define FUNCOES_H
 #define BUFFER_SIZE 1024
+#define FICHEIRO_SIZE 50
 
+
+// Auxiliares
+void write_string(const char *str);
+void read_string(char *buffer, size_t size);
+void read_int(int *number);
+
+// Ações
 void mostraFicheiro(const char *ficheiro);
 void copiaFicheiro(const char *ficheiro);
 void acrescentaFicheiro(const char *origem, const char *destino);
